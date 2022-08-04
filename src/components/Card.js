196b448be11f307eac7f5f4167ec8dcd1f74360d
cfarn <cinniefarnaud@gmail.com>
2022-08-04 10:24:49 +0200

    03 - img

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 const Card = (props) => {
   return (
     <div>
-      <img src={`url('https://image.tmdb.org/t/p/w300${props.poster_path}')`} alt={props.title}/>
+      <img src={`https://image.tmdb.org/t/p/w300${props.poster_path}`} alt={props.title}/>
       <h2>{props.title}</h2>
       <h5>{props.release_date}</h5>
       <p>{props.overview}</p>
